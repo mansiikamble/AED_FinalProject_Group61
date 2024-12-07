a180db738eb73d71ba4d921d89d095e5d3b74054
mitra <mitra@Aditya-Mitra.local>
2024-12-07 16:00:10 -0500

    System Admin Updated

--- HealthcareManagement/src/Business/Role/SystemAdminRole.java
+++ HealthcareManagement/src/Business/Role/SystemAdminRole.java
@@ -3,11 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Business.Role;
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
 
+import javax.swing.JPanel;
+import userinterface.SystemAdminWorkAreas.SystemAdminWorkAreaJPanel;
 /**
  *
  * @author mitra
  */
-public class SystemAdminRole {
+public class SystemAdminRole extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem system) {
+        return new SystemAdminWorkAreaJPanel(userProcessContainer, system);
+    }
     
 }
