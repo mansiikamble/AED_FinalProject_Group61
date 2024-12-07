5d93cd4c33b946d34654562dce5834afff3c6bf7
mansiikamble <kamble.ma@northeastern.edu>
2024-12-07 14:43:55 -0500

    Hospital Enterprise Updated

--- HealthcareManagement/src/Business/Enterprise/HospitalEnterprise.java
+++ HealthcareManagement/src/Business/Enterprise/HospitalEnterprise.java
@@ -4,10 +4,26 @@
  */
 package Business.Enterprise;
 
+import Business.Role.ClinicAdminRole;
+import Business.Role.Role;
+import java.util.ArrayList;
+import java.util.HashSet;
 /**
  *
  * @author mansi kamble
  */
-public class HospitalEnterprise {
+public class HospitalEnterprise extends Enterprise{
+
+    public HospitalEnterprise(String name) {
+        super(name, EnterpriseType.Hospital);
+    }
+
+    @Override
+    public HashSet<Role> getSupportedRole() {
+        roles.add(new ClinicAdminRole());
+        return roles;
+    }
+    
+    
     
 }
