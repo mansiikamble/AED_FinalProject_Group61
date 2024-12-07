33e1864db95a045a49264222cfd401cb50aa1e98
mitra <mitra@Aditya-Mitra.local>
2024-12-07 15:45:48 -0500

    End user role updated

--- HealthcareManagement/src/Business/Role/EndUserRole.java
+++ HealthcareManagement/src/Business/Role/EndUserRole.java
@@ -4,10 +4,25 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.ClinicOrganization;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.EndUser.EndUserWorkAreaJPanel;
+
 /**
  *
  * @author mitra
  */
-public class EndUserRole {
+
+public class EndUserRole extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+       return new EndUserWorkAreaJPanel(userProcessContainer, account, (ClinicOrganization) organization, enterprise, business);
+        
+    }
     
 }
