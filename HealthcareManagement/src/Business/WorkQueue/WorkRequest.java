7ef48fac3763d383a6a62c816cc938df43f2fcfa
mansiikamble <kamble.ma@northeastern.edu>
2024-12-07 17:16:37 -0500

    Work Request Created

---
+++ HealthcareManagement/src/Business/WorkQueue/WorkRequest.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.WorkQueue;
+import Business.Blood.Blood;
+import Business.Clinic.Doctor;
+import Business.Supplier.Vaccine;
+import Business.UserAccount.UserAccount;
+import java.util.Date;
+/**
+ *
+ * @author mansi kamble
+ */
+public abstract class WorkRequest {
+
+    private String message;
+    private UserAccount sender;
+    private UserAccount receiver;
+    private String status;
+    private Date requestDate;
+    private Date resolveDate;
+    private Vaccine vaccine;
+    private Doctor doctor;
+    private Blood blood;
+
+    public Blood getBlood() {
+        return blood;
+    }
+
+    public void setBlood(Blood blood) {
+        this.blood = blood;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+    
+    
+    
+    public WorkRequest(){
+        requestDate = new Date();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public UserAccount getSender() {
+        return sender;
+    }
+
+    public Vaccine getVaccine() {
+        return vaccine;
+    }
+
+    public void setVaccine(Vaccine vaccine) {
+        this.vaccine = vaccine;
+    }
+
+    public void setSender(UserAccount sender) {
+        this.sender = sender;
+    }
+
+    public UserAccount getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(UserAccount receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public Date getResolveDate() {
+        return resolveDate;
+    }
+
+    public void setResolveDate(Date resolveDate) {
+        this.resolveDate = resolveDate;
+    }
+    @Override
+    public String toString(){
+        return status;
+    }
+}
+
