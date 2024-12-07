ce4938f70f4526804225c8a7df6f3656be8d1ff5
mansiikamble <kamble.ma@northeastern.edu>
2024-12-07 17:01:38 -0500

    CDC work request created

---
+++ HealthcareManagement/src/Business/WorkQueue/CDCWorkRequest.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.WorkQueue;
+
+/**
+ *
+ * @author mansi kamble
+ */
+public class CDCWorkRequest extends WorkRequest{
+ 
+    private boolean add;
+    
+    public CDCWorkRequest()
+    {
+        add = false;
+    }
+
+    public boolean isAdd() {
+        return add;
+    }
+
+    public void setAdd(boolean add) {
+        this.add = add;
+    }
+    
+    
+}
